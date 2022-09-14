0667800a4e17d31cb73039de550ec902bee3eb57
Giuseppe Brusco <giuseppebrusco98@gmail.com>
2022-09-14 15:06:48 +0200

    exercise 16

--- Section 14/exercise15.js
+++ Section 14/exercise15.js
@@ -1,7 +1,2 @@
-// Define a variable called myLuckyNumber and set it equal to 37
 let myLuckyNumber = 37;
 let octopusLimbs = 8;
-
-// Alternative Solution
-var myLuckyNumber = 37;
-var octopusLimbs = 8;
